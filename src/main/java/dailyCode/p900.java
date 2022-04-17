6f81ccae92b0675cd420185a83ab2a7b7ac3767c
Javen <834524324@qq.com>
2022-04-17 22:59:29 +0800

    merge windows & mac

--- src/main/java/dailyCode/p900.java
+++ src/main/java/dailyCode/p900.java
@@ -2,4 +2,20 @@ package dailyCode;
 
 public class p900 {
 
+    // p917 仅仅反转字母｛双指针｝
+    public String reverseOnlyLetters(String s) {
+        char[] arr = s.toCharArray();
+        int i = 0, j = arr.length - 1;
+        while (i < j) {
+            while (i < arr.length && !Character.isLetter(arr[i])) i++;
+            while (j >= 0 && !Character.isLetter(arr[j])) j--;
+            if (i == arr.length || j == -1) break;
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(arr);
+    }
 }
