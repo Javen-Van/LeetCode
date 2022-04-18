e2d69be2fd82fb63fa622bcd33fa214fbfe7f858
Javen <834524324@qq.com>
2022-04-18 17:25:46 +0800

    华为面试

--- src/main/java/dailyCode/p300.java
+++ src/main/java/dailyCode/p300.java
@@ -181,6 +181,22 @@ public class p300 {
         return 1;
     }
 
+    // p386 字典序排数
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> res = new ArrayList<>();
+        int cur = 1;
+        for (int i = 0; i < n; i++) {
+            res.add(cur);
+            if (cur * 10 <= n) {
+                cur *= 10;
+            } else {
+                while (cur % 10 == 9 || cur + 1 > n) cur /= 10;
+                cur++;
+            }
+        }
+        return res;
+    }
+
     // p393 UTF-8编码验证
     public boolean validUtf8(int[] data) {
         int n = data.length;
