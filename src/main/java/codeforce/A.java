10cde71f383efb976a44e96622eccb19823cf1f6
Javen <834524324@qq.com>
2022-09-04 10:20:30 +0800

    codeforces

---
+++ src/main/java/codeforce/A.java
@@ -0,0 +1,14 @@
+package codeforce;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            System.out.println(n + 2 * (n / 2 + n / 3));
+        }
+    }
+}
