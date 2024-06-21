3fa76717de952f379fa79e4d94187a80e66ca8a5
duoduo <834524324@qq.com>
2024-06-21 22:19:45 +0800

    20240621 LCP61

---
+++ src/main/java/dailyCode/LCP.java
@@ -0,0 +1,43 @@
+package dailyCode;
+
+public class LCP {
+
+    /**
+     * 气温变化趋势
+     *
+     * @param temperatureA
+     * @param temperatureB
+     * @return
+     */
+    public int temperatureTrend(int[] temperatureA, int[] temperatureB) {
+        int n = temperatureA.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (temperatureA[i] < temperatureA[i + 1]) {
+                temperatureA[i] = 1;
+            } else if (temperatureA[i] == temperatureA[i + 1]) {
+                temperatureA[i] = 0;
+            } else {
+                temperatureA[i] = -1;
+            }
+            if (temperatureB[i] < temperatureB[i + 1]) {
+                temperatureB[i] = 1;
+            } else if (temperatureB[i] == temperatureB[i + 1]) {
+                temperatureB[i] = 0;
+            } else {
+                temperatureB[i] = -1;
+            }
+        }
+        int count = 0, res = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (temperatureA[i] == temperatureB[i]) {
+                count++;
+            } else {
+                res = Math.max(res, count);
+                count = 0;
+            }
+        }
+        res = Math.max(res, count);
+        return res;
+    }
+
+}
